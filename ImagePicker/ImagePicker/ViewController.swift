5490d36ee48486c56d8ac1d8d4e049b0d6ad7774
Jayahari Vavachan <jayahariv@Jayaharis-MacBook-Pro.local>
2018-04-10 08:12:09 -0700

    image picker works done

--- ImagePicker/ImagePicker/ViewController.swift
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -20,6 +20,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func experiment(_ sender: UIButton) {
+//        Image Picker View
+//        let nextViewController = UIImagePickerController()
+//        present(nextViewController, animated: true, completion: nil)
+     
+//        Activity View Controller
+//        let image = UIImage()
+//        let vc = UIActivityViewController.init(activityItems: [image], applicationActivities: nil)
+//        present(vc, animated: true, completion: nil)
+        
+        let vc = UIAlertController.init(title: "title", message: "message", preferredStyle: .actionSheet)
+        let okAction = UIAlertAction.init(title: "OK", style: .default) {[weak self] (action) in
+            self?.dismiss(animated: true, completion: nil)
+        }
+        vc.addAction(okAction)
+        present(vc, animated: true, completion: nil)
+    }
+    
 }
 
