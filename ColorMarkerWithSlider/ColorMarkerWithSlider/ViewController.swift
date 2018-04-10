78c149dcd74871b86346ab5ce3f3522b6168643d
Jayahari Vavachan <jayahariv@Jayaharis-MacBook-Pro.local>
2018-04-10 00:10:05 -0700

    slider done

--- ColorMarkerWithSlider/ColorMarkerWithSlider/ViewController.swift
+++ ColorMarkerWithSlider/ColorMarkerWithSlider/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var displayView: UIView!
+    enum Slider: Int { case red = 0, green, blue};
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        displayView.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green:  CGFloat(greenSlider.value), blue:  CGFloat(blueSlider.value), alpha: 1.0)
     }
-
 
 }
 
