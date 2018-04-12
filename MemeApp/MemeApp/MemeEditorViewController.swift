fc9939c942a3c2082bdd31430e78269e77ee5f14
Jayahari Vavachan <jayahariv@Jayaharis-MacBook-Pro.local>
2018-04-12 00:47:17 -0700

    editor page structure added

--- MemeApp/MemeApp/MemeEditorViewController.swift
+++ MemeApp/MemeApp/MemeEditorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MemeEditorViewController.swift
 //  MemeApp
 //
 //  Created by Jayahari Vavachan on 4/12/18.
@@ -8,18 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MemeEditorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
