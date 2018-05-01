1ebda2a9aaa8f6a37c3a1941a2856ee1d4c22087
Jayahari Vavachan <jayahariv88@gmail.com>
2018-05-01 16:05:03 -0700

    feat: Add Login Screen
    * add launch screen
    * add login screen
    * layout corrected
    * image assets imported

---
+++ OnTheMap/OnTheMap/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Jayahari Vavachan on 5/1/18.
+//  Copyright © 2018 JayahariV. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
