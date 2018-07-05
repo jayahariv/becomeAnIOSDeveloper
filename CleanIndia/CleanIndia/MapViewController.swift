f1d880dba87c35a066fb96d2344141400f1ddae6
Jayahari Vavachan <jayahariv88@gmail.com>
2018-07-05 15:41:50 -0700

    refac: Search Bar Rethink
    * rethink to implement the searchbar, but will keep it as a textfield itself for now.
    * will focus on adding toilet and getting it displayed in map will take priority.

---
+++ CleanIndia/CleanIndia/MapViewController.swift
@@ -0,0 +1,44 @@
+//
+/*
+MapViewController.swift
+Created on: 7/3/18
+
+Abstract:
+this class will show the map with toilet annotations. Also includes the capabilities like
+ - add new toilet.
+ - show the existing toilets as list.
+ - show the menu for more country wise details.
+ - show my location on map.
+ - search for an address.
+*/
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    // MARK: Properties
+
+    @IBOutlet weak var addressTextField: UITextField!
+    let searchResultsDatasource: AddressDatasource = AddressDatasource()
+
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Button Actions
+    
+    @IBAction func onTouchUpMenu(_ sender: UIButton) {
+    }
+    
+    @IBAction func onTouchMyLocation(_ sender: UIButton) {
+    }
+    
+    @IBAction func onTouchUpList(_ sender: UIButton) {
+    }
+    
+    @IBAction func onTouchUpAdd(_ sender: UIButton) {
+    }
+    
+}
