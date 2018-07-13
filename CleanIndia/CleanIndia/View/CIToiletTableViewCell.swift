e9ec684c584b48642c7e880e1a5a84faf858e508
Jayahari Vavachan <jayahariv88@gmail.com>
2018-07-12 17:41:37 -0700

    feat: Add Toilet List
    * fetch data from Remote Firestore
    * show them as a table

---
+++ CleanIndia/CleanIndia/View/CIToiletTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+/*
+CIToiletTableViewCell.swift
+Created on: 7/12/18
+
+Abstract:
+TODO: Purpose of file
+
+*/
+
+import UIKit
+
+class CIToiletTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
