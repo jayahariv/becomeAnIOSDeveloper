ce38ad980e221522f042f4bb2444bdff95366da2
Jayahari Vavachan <jayahariv88@gmail.com>
2018-07-12 19:18:56 -0700

    feat: Add Annotations
    * add annotations to the map
    * validate the data from server before adding as annotation

--- CleanIndia/CleanIndia/MapViewController.swift
+++ CleanIndia/CleanIndia/MapViewController.swift
@@ -81,13 +81,39 @@ private extension MapViewController {
      */
     func fetchToilets() {
         db = Firestore.firestore()
-        db.collection(Constants.Firestore.Keys.TOILETS).getDocuments() { (querySnapshot, err) in
+        db.collection(Constants.Firestore.Keys.TOILETS).getDocuments() { [unowned self] (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
             } else {
+                var annotations = [MKPointAnnotation]()
                 for document in querySnapshot!.documents {
-                    print("\(document.documentID) => \(document.data())")
+                    let data = document.data()
+                    
+                    // GUARD: valid coordinate present
+                    guard let coordinate: GeoPoint = data[Constants.Firestore.Keys.COORDINATE] as? GeoPoint else {
+                        continue
+                    }
+                    
+                    // GUARD: valid name is present
+                    guard let title = data[Constants.Firestore.Keys.NAME] as? String else {
+                        continue
+                    }
+                    
+                    // GUARD: valid address is present
+                    guard let subtitle = data[Constants.Firestore.Keys.ADDRESS] as? String else {
+                        continue
+                    }
+                    
+                    
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = CLLocationCoordinate2DMake(coordinate.latitude, coordinate.longitude)
+                    annotation.title = title
+                    annotation.subtitle = subtitle
+                    
+                    annotations.append(annotation)
                 }
+                
+                self.mapView.addAnnotations(annotations)
             }
         }
     }
@@ -95,6 +121,9 @@ private extension MapViewController {
     /**
      sets up the whole UI configurations in this function
         - focus the map to India
+     - todo:
+        - check if we can gray out the outside area.
+        - auto zoom to the user location when he enabled the location.
      */
     func configureUI() {
         let span = MKCoordinateSpan(latitudeDelta: Constants.India.FullViewCoordinates.delta,
